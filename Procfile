034cf2d18731902eacac575b67d7e2fda8f1afc8
rohandarekar2805 <rohandarekar24@gmail.com>
2026-04-08 18:01:10 +0530

    Ready for deployment

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn laptop_price.wsgi
--- laptop_price/settings.py
+++ laptop_price/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-1_nz=+8=q)(37egc4x=c*)3m#5_i)+nfl&0@)dfenht&(6^-@b
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -118,6 +118,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
